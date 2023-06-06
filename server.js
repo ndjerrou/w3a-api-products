20a18eda5c368da1c79c5a42b0db01ac99771060
ndjerrou <n.djerrou@gmail.com>
2023-06-06 11:33:57 +0100

    Added PUT + DELETE /products

--- server.js
+++ server.js
@@ -31,13 +31,55 @@ app.post('/api/v1/products', (req, res) => {
 
   const totalProducts = products.length;
 
-  const product = { ...req.body, id: (totalProducts + 1).toString() };
+  const product = { ...req.body, id: totalProducts + 1 };
 
   products.push(product);
 
   writeData(products);
 
   res.status(201).send({ ok: true, data: product });
+});
+
+app.put('/api/v1/products/:id', (req, res) => {
+  const products = readData();
+
+  const { id } = req.params;
+
+  const product = products.find((product) => product.id === +id);
+
+  if (!product)
+    return res.status(404).send({ ok: false, msg: 'Invalid id provided' });
+
+  for (let key in req.body) {
+    product[key] = req.body[key];
+  }
+
+  const idx = products.findIndex((product) => product.id === +id);
+
+  products.splice(idx, 1, product);
+
+  writeData(products);
+
+  res.send({ ok: true, data: product });
+});
+
+app.delete('/api/v1/products/:id', (req, res) => {
+  const products = readData();
+
+  const { id } = req.params;
+
+  const product = products.find((product) => product.id === +id);
+
+  if (!product)
+    return res.status(404).send({ ok: false, msg: 'Invalid id provided' });
+
+  const idx = products.findIndex((product) => product.id === +id);
+
+  const deletedProduct = products.splice(idx, 1);
+
+  writeData(products);
+
+  res.send({ ok: true, data: deletedProduct });
 });
 
 // 3 endpoints missing...
